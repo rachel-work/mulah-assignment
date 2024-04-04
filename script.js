142d001062c659d17fd0e1e554f0343f1a756dbb
rachel-work <165910285+rachel-work@users.noreply.github.com>
2024-04-04 12:51:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 window.onload = () => {
     var table = document.getElementById("table");
-    fetch("Table_Input.csv")
-        .then(res => res.text())
-        .then(csv => {
-            table.innerHTML = "";
-            let rows = csv.split("\r\n");
-            for (let row of rows) {
-                let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
-                if (cols != null) {
-                    let tr = table.insertRow();
-                    for (let col of cols) {
-                        let td = tr.insertCell();
-                        td.textContent = col.replace(/(^"|"$)/g, "");
+    if (table) {
+        fetch("Table_Input.csv")
+            .then(res => res.text())
+            .then(csv => {
+                table.innerHTML = "";
+                let rows = csv.split("\r\n");
+                for (let row of rows) {
+                    let cols = row.match(/(?:\"([^\"]*(?:\"\"[^\"]*)*)\")|([^\",]+)/g);
+                    if (cols != null) {
+                        let tr = table.insertRow();
+                        for (let col of cols) {
+                            let td = tr.insertCell();
+                            td.textContent = col.replace(/(^"|"$)/g, "");
+                        }
                     }
                 }
-            }
-        })
-        .catch(error => {
-            console.error('Error loading file:', error);
-        });
+            })
+            .catch(error => {
+                console.error('Error loading file:', error);
+            });
+    } else {
+        console.error('Table element not found.');
+    }
 };
-
-
